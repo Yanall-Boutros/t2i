4786e9378ce9095fd1d04b3c082af1253ece21ce
Yanall-Boutros <YanallBoutros@Protonmail.com>
2024-06-05 16:11:15 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages = {
+          myapp = mkPoetryApplication { projectDir = ./.; };
+          default = self.packages.${system}.myapp;
+        };
+
+        # Shell for app dependencies.
+        #
+        #     nix develop
+        #
+        # Use this shell for developing your app.
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+            export LD_LIBRARY_PATH=${pkgs.cudaPackages.cuda_nvrtc}/lib
+            export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+            export EXTRA_CCFLAGS="-I/usr/include"
+          '';
+          packages = with pkgs; [
+            python311
+            python311Packages.pip
+            python311Packages.matplotlib
+            python311Packages.torch-bin
+            python311Packages.opencv4
+            libtorch-bin
+            cudaPackages.cudnn
+            cudaPackages.cudatoolkit
+            cudaPackages.cuda_nvrtc
+            cudaPackages.libcusparse
+          ];
+          inputsFrom = [ 
+            self.packages.${system}.myapp
+          ];
+        };
+
+        # Shell for poetry.
+        #
+        #     nix develop .#poetry
+        #
+        # Use this shell for changes to pyproject.toml and poetry.lock.
+        devShells.poetry = pkgs.mkShell {
+          packages = [ pkgs.poetry ];
+        };
+      });
+}
